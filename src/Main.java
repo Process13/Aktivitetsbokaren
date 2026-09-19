ebd2ce1b20a4b5867d7df4b21a7aa04f17b7e259
Linus Båge <linus.bage@yh.nackademin.se>
2026-09-19 15:36:48 +0200

    I hela programmet har kommentarer som beskriver vad som händer läggs till.
    ====I Main====
    
    Jag har en statisk ArrayList i main som sparar i RAM. Entt statiskt filnamn anger Textfilens namn där allt även sparas.
    
    startSavedTxtToList anroppas när programmet börjar och läser in från textfilen, avkodar det från sträng till byte/int och skriver in det i början av minnet då den anroppas när programmet börjar och ArrayList är tom.
    
    Några simpla fördydliganden i meny metoden
    
    prisBeräkning retunerar inte längre String. Den retunerar nu Int då detta krävs av uppgiften för att kunna anroppa Konstruktorn i Booking på rätt sätt.
    
    registreraBokning är nu skriven till vad som borde fungera. Den ber om inskrivningar och tar in värden för vilken aktivitet och antal deltagare.
    Aktivitetsnummret anger vilken aktivitetStr (aktivitets sträng) som ska sparas.
    s tilldelas den slutliga strängen som först skrivs ut som programmet vill att man gör, sedan sparas bokningen i både RAM med ArrayList (new Booking) och statFilnamn via skrivLNTillFil.
    
    Onödiga kommentarer som var gjorda för att förklara hur jag planerade spara programmet är borttagen då det inte längre är relevant.
    
    Onödiga metoder jag hade satt in ifall jag behövde dem är borttagna då jag endast behövde en av dessa.
    
    =================
    ====I Booking====
    Aktivitetsnummer ändrat till byte för att matcha ändringar i Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.awt.print.Book;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,58 +12,165 @@ public class Main {
 // - En metod som visar alla registrerade bokningar.
 // - En metod som visar sammanställningen.
 
-    public static void main(String[] args) {
+    static List<Booking> aktivtBokningsminne = new ArrayList<>(); //Aktiva databasen i RAM. Så länge programmet körs är den tillgänglig
+    static String statFilnamn = "SparadeBokningar.txt";
 
-
-        Booking test = new Booking(1, 14, 800);
-        int i = test.getAktivitetsnummer();
-        int j = test.getAntalDeltagare();
-        int k = test.getSlutpris();
-        System.out.println(i + " " + j + " " + k);
-
+    public void main(String[] args) {
+        //Metod för att läsa in bokningar från .txt till ArrayList booking
+        startSavedTxtToList();
+        //Kalla på meny
+        meny();
 
     }
+
+    //Sparar allt innehåll i textfilen in i ArrayList för Booking
+    private void startSavedTxtToList(){
+        String rad;
+        String radAktivitet
+        byte aktivitet;
+        int deltagare;
+        int pris;
+        try {
+            Scanner fil = new Scanner(new File(statFilnamn));
+            while (fil.hasNext()) {     //Går igenom varige rad i filen och konverterar Sträng raderna så att konstruktorn kan ta emot dem
+                rad = fil.nextLine();
+                radAktivitet = (rad.substring(0, rad.indexOf(',')-1).trim());
+                if (radAktivitet.equals("Programmeringsworkshop")) {    //Omvandlar Strängen "Programmeringsworkshop" till dess värde för att sparas
+                    aktivitet = 1;
+                } else if (radAktivitet.equals("Matlagningskurs")) {
+                    aktivitet = 2;
+                } else if (radAktivitet.equals("Träningspass")) {
+                    aktivitet = 3;
+                } else {
+                    throw new IllegalArgumentException("Det är inte korrekt innehåll i filen på raden");
+                }
+                deltagare = Integer.parseInt(rad.substring(rad.indexOf(',')+1, rad.lastIndexOf(',')-9).trim()); //-9 då "deltagare" inte ska med
+                pris = Integer.parseInt(rad.substring(rad.lastIndexOf(',')+1, rad.lastIndexOf('k')).trim());
+                aktivtBokningsminne.add(new Booking(aktivitet, deltagare, pris)); //Konstruktorn anroppas och det sparade värdet lagras
+            }
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+    }
+
     //Metod som visar menyn
     void meny(){
         byte menyVal = 0;
         while(true){
             //Skriv ut meny
             System.out.print("AKTIVITETSBOKAREN\n" + "1. Registrera bokning\n" + "2. Visa alla bokningar\n" + "3. Visa sammanställning\n" + "4. Avsluta");
+
             //Läs av menyval
-            Scanner scan = new Scanner(System.in);
+            while (true) {
+                Scanner scan = new Scanner(System.in);
+                if (scan.hasNextByte()) {
+                    menyVal = scan.nextByte();
+                    break;
+                }
+            }
+
             //if 4 break
+            if (menyVal==4){
+                break;
+            }
 
             //else if 1, skapa bokning
+            else if (menyVal==1) {
+                registreraBokning();
+            }
 
             //else if 2, visa alla bokningar
+            else if (menyVal==2) {
+                // ///////////////////////////////////TOM////////////////////////
+            }
 
             //else if 3, visa sammanställning
+            else if (menyVal==3) {
+                // ///////////////////////////////////TOM////////////////////////
+            }
 
             //else ogiltigt, försök igen.
+            else {
+                System.out.println("\nOgiltigt alternativ, försök igen");
+            }
         }
 
     }
 
     //Metod för att beräkna priset för en aktivitet med ett antal deltagare
-    //Returnerar String för att förhindra att värdet ändras till något annat
-    private String prisBeräkning(byte aktivitet, int deltagare){
-        String prisTxt;
+    private int prisBeräkning(byte aktivitet, int deltagare){
+        int pris; //Int då inga priser innehåller decimaler
         if (aktivitet==1) { //Programmeringsworkshop
-            return prisTxt = 200*deltagare + " kr"; //för 10 deltagare blir det "2000 kr"
+            return pris = 200*deltagare; //för 10 deltagare blir det "2000 kr"
         } else if (aktivitet==2) { //Matlagningskurs
-            return prisTxt = 300*deltagare + " kr";
+            return pris = 300*deltagare;
         } else if (aktivitet==3) { //Träningspass
-            return prisTxt = 150*deltagare + " kr";
+            return pris = 150*deltagare;
         } else {
             throw new IllegalArgumentException("Aktiviteten finns inte");
         }
     }
 
     //Metod för att registrera bokning
-    private void registreraBokning(){
-        //Ta emot bokningsinforrmation
+    private void registreraBokning(){   //Ta emot bokningsinforrmation
+        byte aktivitet;
+        int deltagare;
+        int pris;
+        String aktivitetStr;
+        String s;
 
-        //anropa konstruktor i metod i booking
+        //Aktivitetsnummer
+        System.out.println("Vilken aktivitet gäller registreringen?\n1. Programmeringsworkshop\n2. Matlagningskurs\n3. Träningspass");
+        while (true) {  //Läser in vilken aktivitet som ska registreras
+            Scanner scanA = new Scanner(System.in);
+            if (scanA.hasNextByte()) {
+                aktivitet = scanA.nextByte();
+                if (aktivitet > 0 && aktivitet <= 3) {  //Är nummret som angavs en aktivitet?
+                    break;
+                } else {
+                    System.out.println("\nAktiviteten finns inte.");
+                }
+            } else {
+                throw new IllegalArgumentException("Aktiviteten finns inte");
+            }
+        }
+
+        //Deltagare
+        System.out.print("Hur många deltagare vill du registrera för aktiviteten?\nAntal: ");
+        while (true){   //Läser in antal deltagare
+            Scanner scanD = new Scanner(System.in);
+            if (scanD.hasNextInt()){
+                deltagare = scanD.nextInt();
+                if (deltagare>0){
+                    break;
+                } else {
+                    System.out.println("\nFel värde för antal deltagare");
+                }
+            } else {
+                throw new IllegalArgumentException("Fel värde för antal deltagare");
+            }
+        }
+
+        //Beräkna bokningens slutpris.
+        pris = prisBeräkning(aktivitet, deltagare);
+
+        //Skriv ut aktivitet, antal deltagare och slutpris när bokningen registreras.
+        if (aktivitet == 1) {    //Aktivitet    byte -> string
+            aktivitetStr = "Programmeringsworkshop";
+        } else if (aktivitet == 2) {
+            aktivitetStr = "Matlagningskurs";
+        } else {
+            aktivitetStr = "Träningspass";
+        }
+        s = aktivitetStr + ", " + deltagare + " deltagare, " + pris + " kr"; //Registreringen i Sträng
+        System.out.println(s);
+
+        //Anropa konstruktorn för Booking
+        aktivtBokningsminne.add(new Booking(aktivitet, deltagare, pris)); //Konstruktorn anroppas
+
+        //Spara registreringen i textfilen
+        skrivLnTillFil(statFilnamn, s);
+
     }
 
     //Metod för att visa alla registrerade bokningar
@@ -72,53 +182,11 @@ public class Main {
         //Skriv ut Listan
 
 
-/*
-    {3, 14, 653}
-    {"1,"  "12" ":summa"}
-
-var bokning = new Booking();
-    {2, 17, ": Namn summa kr"}
-    {1, 5, ": Namn summa kr"}
-    {3, 14, ": Namn summa kr"}
-    {3, 14, ": Namn summa kr"}
-
-ArrayList(new Booking)
-
-
- */
-
-
-    //Läser och retunerar en rad från en fil.     Blir null om inget hittades
-    public static String läsRadFrånFil(String filnamn, int rad){
-        try {
-            String returRad = null;
-            BufferedReader läsFil = new BufferedReader(new FileReader(filnamn));
-            for (int i = 0; i<rad; i++){
-                returRad = läsFil.readLine();
-            }
-            return returRad;
-        } catch (IOException e) {
-            System.out.println("Error");
-            return null;
-        }
-    }
-
     //Skriver en rad text till en fil
-    static void SkrivLnTillFil(String filnamn, String text){
+    public static void skrivLnTillFil(String filnamn, String text){
         try {
             PrintWriter skriv = new PrintWriter(new BufferedWriter(new FileWriter(filnamn, true)));
             skriv.println(text);
-        }catch (IOException e){
-            System.out.println("Error");
-        }
-    }
-
-    //Skriver text till en fil
-    static void SkrivTillFil(String filnamn, String text){
-        try {
-            PrintWriter skriv = new PrintWriter(new BufferedWriter(new FileWriter(filnamn, true)));
-            skriv.print(text);
-            skriv.close();
         }catch (IOException e){
             System.out.println("Error");
         }
